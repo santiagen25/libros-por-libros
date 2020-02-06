857c8aa55c10e9e94fa28dfb0b0b59e3924364bb
Santiago Torrabadella Ferrer <santiago.torrabadella@gracia.lasalle.cat>
2020-02-07 00:33:03 +0100

    intento de responsive en inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -13,10 +13,10 @@
     <div class="d-flex justify-content-center">
         <div class="enmarcarCuadrado container">
             <div class="enmarcarNoticia row">
-                <div class="col-md-9 my-auto">
+                <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 col-xl-9 my-auto">
                     <h4>Has leido Libro 1</h4>
                 </div>
-                <div class="col-md-3">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
                     <div class="row my-auto">
                         <p>fecha</p>
                     </div>
@@ -25,7 +25,7 @@
                     </div>
                 </div>
             </div>
-            <div class="enmarcarNoticia row">
+            <!--div class="enmarcarNoticia row">
                 <div class="col-md-9 my-auto">
                     <h4>Has leido Libro 2</h4>
                 </div>
@@ -76,7 +76,7 @@
                         <p>likes</p>
                     </div>
                 </div>
-            </div>
+            </div-->
         </div>
     </div>
 @endsection
