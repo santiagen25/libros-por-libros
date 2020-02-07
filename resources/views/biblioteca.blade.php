97278a2deba290fbd3820760552de93a3ebfe401
Santiago Torrabadella Ferrer <santiago.torrabadella@gracia.lasalle.cat>
2020-02-07 11:44:33 +0100

    responsive images

--- resources/views/biblioteca.blade.php
+++ resources/views/biblioteca.blade.php
@@ -12,7 +12,52 @@
 @section('content')
     <div class="d-flex justify-content-center">
         <div class="enmarcarCuadrado container">
-            mis libros
+        <div class="row m-4">
+                <div class="mr-4 col-md-3">
+                    <img src="{{asset('images\cumbres_borrascosas_portada.jpg')}}" class="rounded img-fluid fotoMiniLista" alt="Foto de Perfil">
+                </div>
+
+                <div class="col-md-8">
+                    <div class="row mb-4 mt-2">
+                        <div class="col-md-4">
+                            <label class="h4">
+                                Nombre:
+                            </label>
+                        </div>
+                        <div class="pt-1 col-md-8">
+                            <p>
+                                Cumbres Borrascosas
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="row mb-4">
+                        <div class="col-md-4">
+                            <label class="h4">
+                                Autor:
+                            </label>
+                        </div>
+                        <div class="pt-1 col-md-8">
+                            <p>
+                                Emily Bronte
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="row mb-4">
+                        <div class="col-md-4">
+                            <label class="h4">
+                                ISBN:
+                            </label>
+                        </div>
+                        <div class="pt-1 col-md-8">
+                            <p>
+                                X
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
